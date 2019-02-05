b96d0571f3d2c8d733797f5441d7e37be12f5e74
Jordan Duran <jordanduranm@gmail.com>
2019-02-05 04:26:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var playlist = {
+var playlist = new Object({keys: '0'});
+
+ function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+ function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
